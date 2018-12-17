91c8856bd6fa7b8281cf87d93bffe678d591361a
pchmc72462 <pchmc72462@gmail.com>
2018-12-17 08:26:49 -0800

    Add front-end logic to set times array in countdown to change every 1000ms.

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,19 @@ function countdown(x) {
   let currentDate = new Date().getTime();
   let countDate = new Date(x).getTime();
   let diff = countDate - currentDate;
-
   let milSecs = [86400000, 3600000, 60000, 1000];
   let times = []
   let days = Math.floor(diff / milSecs[0]);
-  console.log(days);
+  times.push(days);
   for (i=0; i<milSecs.length-1; i++) {
     let a = Math.floor(diff % milSecs[i]/milSecs[i+1]);
-    console.log(a);
+    times.push(a);
   }
+  console.log(times);
 }
 
-countdown("Dec 25, 2018 00:00:00");
+window.onload = function frontEnd() {
+  setInterval(function() {
+    countdown("Dec 25, 2018 00:00:00");
+  }, 1000);
+}
