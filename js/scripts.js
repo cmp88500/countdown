55c62e61e3cd63f3b736581fe575f23eaee236a4
pchmc72462 <pchmc72462@gmail.com>
2018-12-17 08:35:07 -0800

    Display countdown and sentence with string concatted. Convert times array to clock format.

--- js/scripts.js
+++ js/scripts.js
@@ -10,11 +10,18 @@ function countdown(x) {
     let a = Math.floor(diff % milSecs[i]/milSecs[i+1]);
     times.push(a);
   }
-  console.log(times);
+  let timeClock = `${times[0]}: ${times[1]}: ${times[2]}: ${times[3]}`;
+  return timeClock;
 }
 
 window.onload = function frontEnd() {
   setInterval(function() {
-    countdown("Dec 25, 2018 00:00:00");
+    function display(time, id, name) {
+      let div = document.getElementById(id);
+      let clock = countdown(time);
+      div.children[0].innerHTML = `Countdown to ${name}`;
+      div.children[1].innerHTML = clock;
+    }
+    display("Dec 25, 2018 00:00:00", 'countdown', 'Christmas');
   }, 1000);
 }
