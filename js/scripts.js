e7a2fc0eaf7c16cd292ca9afe9799b8d692a5f52
pchmc72462 <pchmc72462@gmail.com>
2018-12-17 08:37:26 -0800

    Add function to add zero to numbers less than 10.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
+function addZero(x) {
+  if (x < 10) {
+    x = `0${x}`;
+    return x;
+  } else {
+    return x;
+  }
+}
+
 function countdown(x) {
   let currentDate = new Date().getTime();
   let countDate = new Date(x).getTime();
   let diff = countDate - currentDate;
   let milSecs = [86400000, 3600000, 60000, 1000];
   let times = []
-  let days = Math.floor(diff / milSecs[0]);
+  let days = addZero(Math.floor(diff / milSecs[0]));
   times.push(days);
   for (i=0; i<milSecs.length-1; i++) {
-    let a = Math.floor(diff % milSecs[i]/milSecs[i+1]);
+    let a = addZero(Math.floor(diff % milSecs[i]/milSecs[i+1]));
     times.push(a);
   }
   let timeClock = `${times[0]}: ${times[1]}: ${times[2]}: ${times[3]}`;
