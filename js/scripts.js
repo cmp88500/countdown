c7f4285833138321247f9da0551d24da821ae777
pchmc72462 <pchmc72462@gmail.com>
2018-12-17 08:07:42 -0800

    Created variables to determine ms in certain time frames. Transfer answers to an array.

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,18 @@ function countdown(x) {
   let currentDate = new Date().getTime();
   let countDate = new Date(x).getTime();
   let diff = countDate - currentDate;
-  console.log(currentDate);
-  console.log(countDate);
-  console.log(diff);
+
+  let seconds = 1000;
+  let minutes = 1000 * 60;
+  let hours  = 1000 * 60 * 60;
+  let days = 1000 * 60 * 60 * 24;
+  console.log(seconds);
+  console.log(minutes);
+  console.log(hours);
+  console.log(days);
+
+  let times = [1000, 60000, 3600000, 86400000];
+  console.log(times);
 }
 
 countdown("Dec 25, 2018 00:00:00");
